8234b49d66b277571c6180c4d11a2bf038cdc6d2
Wisdom Akpasa <wisdomnemesis@gmail.com>
2025-02-10 15:31:24 +0100

    Added build definitions to vite.config.ts

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -5,4 +5,14 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "build",
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
+  },
 })
